b51975bbc78111ffc7271135507dd64721002302
netelli <natalli.dev@gmail.com>
2019-09-04 17:26:14 +0300

    Use @Qualifier annotation;

--- spring-anotations/src/main/java/com/study/springdemo/TennisCoach.java
+++ spring-anotations/src/main/java/com/study/springdemo/TennisCoach.java
@@ -1,20 +1,17 @@
 package com.study.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
+    @Autowired
+    @Qualifier("randomFortuneService")
     private FortuneService fortuneService;
 
     public TennisCoach() {
         System.out.println(">> TennisCoach: inside default constructor");
-    }
-
-    @Autowired
-    public void doSomeStuff(FortuneService fortuneService) {
-        System.out.println(">> TennisCoach: inside doSomeStuff() method");
-        this.fortuneService = fortuneService;
     }
 
     @Override
