9fa9c84c075bbfa53314a208a5d7e7889ded4dd8
netelli <natalli.dev@gmail.com>
2019-09-09 17:19:43 +0300

    Spring configuration with Java code via @ComponentScan annotation;

---
+++ spring-anotations/src/main/java/com/study/springdemo/SportConfig.java
@@ -0,0 +1,9 @@
+package com.study.springdemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.study.springdemo")
+public class SportConfig {
+}
