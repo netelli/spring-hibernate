a7ce3f9ac4f4053da53c5e572c4b81b4de8c050d
netelli <natalli.dev@gmail.com>
2019-09-11 12:02:31 +0300

    Defining Spring Beans with Java code, @Bean annotation;

--- spring-anotations/src/main/java/com/study/springdemo/SportConfig.java
+++ spring-anotations/src/main/java/com/study/springdemo/SportConfig.java
@@ -1,9 +1,20 @@
 package com.study.springdemo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.study.springdemo")
+//@ComponentScan("com.study.springdemo")
 public class SportConfig {
+
+    @Bean
+    public FortuneService sadFortuneService() {
+        return new SadFortuneService();
+    }
+
+    @Bean
+    public Coach swimCoach() {
+        return new SwimCoach(sadFortuneService());
+    }
 }
